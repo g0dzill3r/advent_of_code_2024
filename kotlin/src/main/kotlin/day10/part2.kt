489807901aee3ece3733a84f36b07432526a683c
Lee Crawford <lee.crawford@gmail.com>
2024-12-10 06:10:21 -0800

    day10, part2

--- kotlin/src/main/kotlin/day10/part2.kt
+++ kotlin/src/main/kotlin/day10/part2.kt
@@ -1,4 +1,26 @@
 package day10
 
-class part2 {
+import util.withInput
+
+
+fun main () {
+    println("day$DAY, part1")
+    withInput(DAY, SAMPLE) { input ->
+        val grid = Grid(input)
+        val trailheads = grid.trailheads
+        println ("Found ${trailheads.size} trailheads")
+
+        val hikes = trailheads.map {
+            grid.getHikes (it)
+        }
+
+        var total = 0
+        hikes.forEachIndexed { i, hike ->
+            total += hike.size
+        }
+        println (total)
+    }
+    return
 }
+
+// EOF
